49b8a07665c6a7ee0234310aac72e4a8e2a64571
wilcar <wilfrid.cariou@gmail.com>
2021-12-20 13:49:29 +0100

    Amélioration de DT

--- app.R
+++ app.R
@@ -7,36 +7,34 @@ library(dplyr)
 library(shiny)
 library(DT)
 library(data.table)
-library(bslib)
+library(shinythemes)
 
 
-# Example data
-
-
-# reading data
+# Lecture des données
 
 
 load("communes_Cassini3.RData")
 dataset <- sort(communes_Cassini3$source)
 dataset2 <- as.data.table(dataset)
 
-# La conversion en objet data.table a pour objectif accélérer l'affichage des noms de communes dans le selectizeInput.
+# La conversion en objet data.table a pour objectif accélérer l'affichage des noms de communes dans selectizeInput.
 
 # UI
 ui <- fluidPage(
-  theme = bs_theme(version = 4, bootswatch = "minty"),
-  # App title ----
-  titlePanel("Moteur de recherche des noms de communes de France"),
+  theme = shinytheme("united"), # Thème
+  titlePanel("Moteur de recherche des toponymes des communes de France"),
   sidebarLayout(
     mainPanel(
-    p("Moteur de recherche de noms de communes basé sur les notices communales de la base Des villages de Cassini aux communes d'aujourd'hui"),
-    p("(La base constituée par la BNF, l'EHESS, le CNRS et l'INED, propose, à partir des cartes de Cassini, associées aux limites administratives actuelles, l'accès aux évolutions des toponymes, des populations et des territoires communaux. "),
+    p("Le moteur de recherche  est basé sur les notices communales de la base Des villages de Cassini aux communes d'aujourd'hui permet d'accéder aux différentes dénominations communales."),
+    p("La base constituée par la BNF, l'EHESS, le CNRS et l'INED, propose, à partir des cartes de Cassini, associées aux limites administratives actuelles,
+      l'accès aux évolutions des toponymes, des populations et des territoires communaux. "),
     p("Auteur: Wilfrid Cariou"),
     p("Date de céation : 2018"),
      # Input(s)
     sidebarPanel(
       selectizeInput(inputId = "commune",  
                   label = "commune",
+                  options = list(placeholder = ''),
                   choices = NULL, # 
                   selected = "")
     )
@@ -49,21 +47,24 @@ ui <- fluidPage(
   )
 )
 
-# performances : 
-# choices = NULLcombiné avec updateSelectize(server = TRUE)
+# Pour conserver de bonnes performances de recherche on combine :
+# choices = NULL  avec updateSelectize(server = TRUE)
 # https://shiny.rstudio.com/articles/selectize.html
 
 # Server
 server <- function(input, output, session) {
   updateSelectizeInput(session = session, inputId = 'commune',
-                       choices = c(Choose = '', dataset2), server = TRUE)
+                       choices = c(`votre recherche...` = '', dataset2), server = TRUE)
   # Create data table
   output$table_de_donnees <- DT::renderDataTable({
     fiche_commune <- communes_Cassini3 %>%
       filter(source == input$commune) %>%
       select(insee:designation_actuelle)
-    DT::datatable(data = fiche_commune, 
-                  options = list(pageLength = 10), 
+    DT::datatable(data = fiche_commune,
+                  colnames=c("code insee", "dpt", "désignation antérieure", "désignation actuelle"), 
+                  options = list(pageLength = 10, dom = 't', # t : uniquement la table. 
+                                 language = list(
+                                   zeroRecords = " ") ),  # Desactiver l'affichage 0 records
                   rownames = FALSE)
   })
   
